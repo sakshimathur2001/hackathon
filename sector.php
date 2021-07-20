74a75389c8f5c4c4b38dcb38935e87e88efb0001
sakshimathur2001 <64127210+sakshimathur2001@users.noreply.github.com>
2021-07-21 02:20:39 +0530

    images updated

--- sector.php
+++ sector.php
@@ -12,13 +12,13 @@
 <div class="sector_list">
     <div class="row rowheight">
     <div class="container column">
-        <img src="images/ecological.png" alt="Enviornment" class="image">
+        <img src="images/enviornment.png" alt="Enviornment" class="image">
         <div class="overlay">
         <div><a class="text" href="ngo_env.php">Enviornment</a></div>
         </div>
     </div>
     <div class="container column">
-    <img src="images/feminine.png" alt="Women's Development and Empowerement" class="image">
+    <img src="images/women.png" alt="Women's Development and Empowerement" class="image">
         <div class="overlay">
         <div><a class="text" href="ngo_wd.php">Women's Development and Empowerement</a></div>
         </div>
@@ -27,13 +27,13 @@
     </div>
     <div class="row rowheight">
     <div class="container column">
-    <img src="images/poverty.png" alt="Urban Development and Poverty Alleviation" class="image">
+    <img src="images/urban.png" alt="Urban Development and Poverty Alleviation" class="image">
         <div class="overlay">
         <div><a class="text" href="ngo_ud.php">Urban Development and Poverty Alleviation</a></div>
         </div>
     </div>
     <div class="container column">
-    <img src="images/protect.png" alt="Animal Husbandry, Dairying and Fisheries" class="image">
+    <img src="images/animal.png" alt="Animal Husbandry, Dairying and Fisheries" class="image">
         <div class="overlay">
         <div><a class="text" href="ngo_ah.php">Animal Husbandry, Dairying and Fisheries</a></div>
         </div>
@@ -41,13 +41,13 @@
     </div>
     <div class="row rowheight">
     <div class="container column">
-    <img src="images/boy.png" alt="Children" class="image">
+    <img src="images/children.png" alt="Children" class="image">
         <div class="overlay">
         <div><a class="text" href="ngo_child.php">Children</a></div>
         </div>
     </div>
     <div class="container column">
-    <img src="images/protection.png" alt="HIV and AIDS" class="image">
+    <img src="images/hiv.png" alt="HIV and AIDS" class="image">
         <div class="overlay">
         <div><a class="text" href="ngo_hiv.php">HIV and AIDS</a></div>
         </div>
@@ -55,15 +55,43 @@
     </div>
     <div class="row rowheight">
     <div class="container column">
-    <img src="images/couple.png" alt="Elderly/Aged" class="image">
+    <img src="images/aged.png" alt="Elderly/Aged" class="image">
         <div class="overlay">
         <div><a class="text" href="ngo_age.php">Elderly/Aged</a></div>
         </div>
     </div>
     <div class="container column">
-    <img src="images/agriculture.png" alt="Agriculture" class="image">
+    <img src="images/agri.png" alt="Agriculture" class="image">
         <div class="overlay">
         <div><a class="text" href="ngo_agri.php">Agriculture</a></div>
+        </div>
+    </div>
+    </div>
+    <div class="row rowheight">
+    <div class="container column">
+    <img src="images/disable.png" alt="Differently Abled" class="image">
+        <div class="overlay">
+        <div><a class="text" href="ngo_dif.php">Differently Abled</a></div>
+        </div>
+    </div>
+    <div class="container column">
+    <img src="images/education.png" alt="Education" class="image">
+        <div class="overlay">
+        <div><a class="text" href="ngo_edu.php">Education</a></div>
+        </div>
+    </div>
+    </div>
+    <div class="row rowheight">
+    <div class="container column">
+    <img src="images/energy.png" alt="New and Renewable Energy" class="image">
+        <div class="overlay">
+        <div><a class="text" href="ngo_energy.php">New and Renewable Energy</a></div>
+        </div>
+    </div>
+    <div class="container column">
+    <img src="images/human.png" alt="Human Rights" class="image">
+        <div class="overlay">
+        <div><a class="text" href="ngo_humr.php">Human Rights</a></div>
         </div>
     </div>
     </div>
