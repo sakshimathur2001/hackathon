7415aaa3988153715dee4e9680717af4e839c154
sakshimathur2001 <64127210+sakshimathur2001@users.noreply.github.com>
2021-07-20 20:08:25 +0530

    second commit

--- index.php
+++ index.php
@@ -1,48 +1,8 @@
 <?php
     $title='Index';
-    //require_once 'includes\header.php'; 
+    include('includes\header.php'); 
     require_once 'db/conn.php';
 ?>
-</head>
-  <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">One Good Deed</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        <a class="nav-link" href="sector.php">Sectors</a>
-        <a class="nav-link" href="#">Contact Us</a>
-        <a class="nav-link" href="#">About Us</a>
-      </div>
-    </div>
-  </div>
-</nav>
-  <div class="text-center">
-
-    <h1>Hello, Home Page!</h1>
-    <a href="sector.php">SECTOR</a>
-    <a href="ngo.php">NGO</a>
-    
-<div style="background-color: #03989e">
-    this is the footer
-</div>
-
-
-<!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-    </div>
-  </body>
-</html>
+<h1>Hello, Home Page!</h1>
+<a href="sector.php">SECTOR</a>
 <?php require_once 'includes\footer.php'; ?>    
