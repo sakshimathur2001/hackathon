01326dfdf28767ebe5246ea79ac71d2f17f4fcbf
sakshimathur2001 <64127210+sakshimathur2001@users.noreply.github.com>
2021-07-20 19:38:29 +0530

    second commit

--- db/conn.php
+++ db/conn.php
@@ -1,10 +1,16 @@
 <?php 
+/*
     $host = 'localhost';
     $db = 'db_ngo';
     $user = 'root';
     $pass = '';
     $charset = 'utf8mb4';
-
+*/
+$host = 'sql6.freemysqlhosting.net';
+    $db = 'sql6426681';
+    $user = 'sql6426681';
+    $pass = 'Rzc2lgWjy6';
+    $charset = 'utf8mb4';
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
     try{
